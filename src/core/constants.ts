95e47a023f8a69f1078f0ca17232a49e08ebe5b8
Marcuth <thiago.playgames15@gmail.com>
2025-04-02 17:18:57 -0300

    atualiza dependências no package.json para versões fixas; ajusta tipo de dados em constantes para suportar BigInt

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -6,9 +6,12 @@ export const defaultFormattedNumberSuffixes = {
     b: 1_000_000_000,
     t: 1_000_000_000_000,
     q: 1_000_000_000_000_000,
-    qq: 1_000_000_000_000_000_000,
-} satisfies Record<string, number>
+    qq: BigInt(1_000_000_000_000_000_000),
+} satisfies Record<string, number | BigInt>
 
 export const defaultUserAgent = "Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
 
 export const defaultPuppeteerActionType = "afterRequest"
+
+
+var desgraça = "vai tomar no cu"
